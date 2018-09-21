db8a8a9a170e113fe734270e2767761512336f2b
Raul Armas Santiago <raularmassantiago@gmail.com>
2018-09-21 09:05:28 -0500

    profile image

--- src/models/users.js
+++ src/models/users.js
@@ -16,7 +16,7 @@ const UserSchema = new Schema({
     },
     "image_url":{
         type:String,
-        default:"https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png"
+        default:"http://www.hussein-ig.com/gui/images/avatar.png"
     },
     "email":{
         type:String,
